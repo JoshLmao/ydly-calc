72fe4fa16cc43e7fba049e7557a46f3996eff146
Josh Shepherd <josh_lmao@live.co.uk>
2021-07-07 20:40:25 +0100

    Tried to make a fully 404 page ;)

--- src/components/FourOhFour/FourOhFour.jsx
+++ src/components/FourOhFour/FourOhFour.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import { Container, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+
+import "../Home/Home.css";
 
 class FourOhFour extends Component {
     render() {
+        let comedyGeniusSentences = [
+            "You tried to hardfork from the main site and failed. Please don't try it again",
+            "This is a standard 404 message. Please return home.",
+        ];
+
+        let funnySentence = comedyGeniusSentences[Math.floor(Math.random() * comedyGeniusSentences.length)];
+
         return (
-            <div>
-                404 bruh
+            <div className="py-5 secondary-background all-text-white" style={{
+                minHeight: "100%"
+            }}>
+                <Container className="py-5">
+                    <h1 className="yieldly-main-color">你迷路了吗</h1>
+                    <p>{ funnySentence }</p>
+                    <Link to="/">
+                        <Button className="d-flex">
+                            <FontAwesomeIcon 
+                                className="my-auto"
+                                icon={faHome} />
+                            <div className="ml-2">
+                                Return home
+                            </div>
+                        </Button>
+                    </Link>
+                </Container>
             </div>
         );
     }
