57556463c23f1e69e16e11bce847e7ce5d2a5d82
Josh Shepherd <josh_lmao@live.co.uk>
2021-08-07 13:31:20 +0100

    Able to sort data by ascending different columns

--- src/components/YLDYAssetStats/TopStakers/TopStakers.jsx
+++ src/components/YLDYAssetStats/TopStakers/TopStakers.jsx
@@ -1,7 +1,8 @@
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faChevronUp, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { Component } from 'react';
-import { Card, Col, Pagination, Row, Table } from 'react-bootstrap';
+import { Card, Col, Pagination, Row, Table, Button } from 'react-bootstrap';
+
 import { constants } from '../../../js/consts';
 import { getAllStakingData } from '../../../js/FirebaseAPI';
 import { shortenAddress, fromMicroFormatNumber} from '../../../js/utility';
@@ -9,22 +10,27 @@ import { shortenAddress, fromMicroFormatNumber} from '../../../js/utility';
 import YLDY_ICON from "../../../svg/yldy-icon.svg";
 import ALGO_ICON from "../../../svg/algo-icon.svg";
 
-const YLDY_ASSET_ID = 226701642;
-
-function tryGetStateValue(addrData, key, defaultVal = 0) {
-    if (addrData && addrData.stateData) {
-        let foundVal = addrData.stateData[key];
-        return isNaN(foundVal) ? defaultVal : foundVal;
+// Tries to get a value from the addrData.
+// First child key should be either "assets" or "stateData"
+// Second child key expected to be child key of firstChildKey
+function tryGetStateValue(addrData, firstChildKey, secondChildKey, defaultVal = 0) {
+    if (addrData) {
+        let firstChild = addrData[firstChildKey];
+        if (firstChild) {
+            let foundVal = firstChild[secondChildKey];
+            return isNaN(foundVal) ? defaultVal : foundVal;
+        }
     }
     return defaultVal;
 }
 
+// Analyse all staking data and get stats
 function analyseStakingData (stakingData) {
     let totalStakedALGO = 0;
     let totalStakedYLDY = 0
     for (let addrData of stakingData.yieldlyData) {
-        totalStakedALGO += tryGetStateValue(addrData, constants.NO_LOSS_LOTTERY_APP_ID);
-        totalStakedYLDY += tryGetStateValue(addrData, constants.YLDY_STAKING_APP_ID);
+        totalStakedALGO += tryGetStateValue(addrData, "stateData", constants.NO_LOSS_LOTTERY_APP_ID);
+        totalStakedYLDY += tryGetStateValue(addrData, "stateData", constants.YLDY_STAKING_APP_ID);
     }
 
     return [
@@ -50,15 +56,27 @@ class TopStakers extends Component {
         super(props);
 
         this.state = {
+            // Original staking data
             stakingData: null,
+            // Staking data sorted
+            sortedStakingData: null,
+            // Sort by code to use
             sortBy: 'staked-yldy',
+            // Is staking data current being loaded?
             loadingStakers: false,
 
+            // Should the staking data be loaded from Firebase?
             useFirebaseData: true,
 
+            // Current active page in table pagination
             pageActive: 0,
+            // How much to segment the data up between pagination
             pageSegment: 1000,
+            // Height of the column header images for ALGO and YLDY 
+            headerImageHeight: 21,
         };
+
+        this.onUpdateSort = this.onUpdateSort.bind(this);
     }
 
     componentDidMount() {
@@ -70,7 +88,49 @@ class TopStakers extends Component {
                 this.setState({
                     stakingData: stakingData,
                     loadingStakers: false,
-                });
+                }, () => this.onUpdateSort() );
+            });
+        }
+    }
+
+    onUpdateSort() {
+        if (this.state.stakingData) {
+            let sorted = [];
+            switch (this.state.sortBy) {
+                case "wallet-algo":
+                    {
+                        sorted = this.state.stakingData.yieldlyData
+                                    .sort((a,b) => tryGetStateValue(b, "assets", "ALGO") - tryGetStateValue(a, "assets", "ALGO"));
+                        break;
+                    }
+                case "wallet-yldy":
+                    {
+                        sorted = this.state.stakingData.yieldlyData
+                                    .sort((a,b) => tryGetStateValue(b, "assets", constants.YLDY_ASSET_ID) - tryGetStateValue(a, "assets", constants.YLDY_ASSET_ID));
+                        break;
+                    }
+                case "staked-algo":
+                    {
+                        sorted = this.state.stakingData.yieldlyData
+                                    .sort((a,b) => tryGetStateValue(b, "stateData", constants.NO_LOSS_LOTTERY_APP_ID) - tryGetStateValue(a, "stateData", constants.NO_LOSS_LOTTERY_APP_ID));
+                        break;
+                    }
+                case "staked-yldy":
+                    {
+                        sorted = this.state.stakingData.yieldlyData
+                                    .sort((a,b) => tryGetStateValue(b, "stateData", constants.YLDY_STAKING_APP_ID) - tryGetStateValue(a, "stateData", constants.YLDY_STAKING_APP_ID));
+                        break;
+                    }
+                default:
+                    {
+                        // Unknown key, use default data
+                        sorted = this.state.stakingData;
+                        break;
+                    }
+            }
+            
+            this.setState({
+                sortedStakingData: sorted,
             });
         }
     }
@@ -78,16 +138,16 @@ class TopStakers extends Component {
     render() {
         let paginationTotal = 0;
         let paginationItms = [];
-        if (this.state.stakingData) {
+        if (this.state.sortedStakingData) {
             // Total is all entries / segmenting of pagination
-            paginationTotal = Math.floor(this.state.stakingData.yieldlyData.length / this.state.pageSegment);
+            paginationTotal = Math.floor(this.state.sortedStakingData.length / this.state.pageSegment);
             // Add 1 for last set of 1000 data entries
             paginationTotal += 1;
             // Create all pagination items
             for (let i = 0; i < paginationTotal; i++) {
                 paginationItms.push(
                     <Pagination.Item 
-                        key={ i }
+                        key={ "pagination-key-" + i }
                         active={ i === this.state.pageActive }
                         onClick={(e) => this.setState({ pageActive: parseInt(e.target.innerText) })}>
                         { i }
@@ -99,19 +159,28 @@ class TopStakers extends Component {
 
         return (
             <div className="py-3">
-                <h3 className="yldy-title">
+                <h2 className="yldy-title">
                     Yieldly Staking Statistics
-                </h3>
+                </h2>
                 <p>
                     A snapshot of all wallets that have opted-in to the YLDY asset and have staked in a Yieldly application on the blockchain. 
                     This data is not live and requires manually updating by myself, 
                     which I aim to do every Friday.
-                    <br />
+                    <br/>
+                    <small className="text-muted">
+                        If the data out of date by more than a week, please send me tweet to remind me! 
+                        <a 
+                            className="mx-2"
+                            href="https://twitter.com/joshlmao">
+                            @JoshLmao
+                        </a>
+                    </small>
+                    <br/>
                     {
                         this.state.stakingData && (
-                            <p>
+                            <>
                                 The current snapshot is from { new Date(this.state.stakingData.snapshotEpoch * 1000).toLocaleString() }
-                            </p>
+                            </>
                         )
                     }
                 </p>
@@ -132,7 +201,7 @@ class TopStakers extends Component {
                     )
                 }
                 {
-                    this.state.stakingData && paginationTotal && (
+                    this.state.sortedStakingData && paginationTotal && (
                         <div className="w-100 text-right">
                             <Pagination>
                                 { paginationItms }
@@ -147,7 +216,7 @@ class TopStakers extends Component {
                     }}
                     >
                     {
-                        this.state.stakingData && (
+                        this.state.sortedStakingData && (
                             <Table
                                 bordered
                                 size="sm"
@@ -158,61 +227,78 @@ class TopStakers extends Component {
                                     <tr>
                                         <th>#</th>
                                         <th>Algorand Address</th>
-                                        <th>
-                                            Wallet: ALGO
-                                            <img 
-                                                className="mx-2"
-                                                src={ALGO_ICON} 
-                                                alt="Algorand asset icon" 
-                                                height="23"
-                                                />
-                                        </th>
-                                        <th>
-                                            Wallet: YLDY
-                                            <img 
-                                                className="mx-2"
-                                                src={YLDY_ICON} 
-                                                alt="Yieldly asset icon" 
-                                                height="23"
-                                                />
-                                        </th>
-                                        <th>
-                                            Staked: ALGO
-                                            <img 
-                                                className="mx-2"
-                                                src={ALGO_ICON} 
-                                                alt="Algorand asset icon" 
-                                                height="23"
-                                                />
-                                        </th>
-                                        <th>
-                                            Staked: YLDY
-                                            <img 
-                                                className="mx-2"
-                                                src={YLDY_ICON} 
-                                                alt="Yieldly asset icon" 
-                                                height="23"
-                                                />
-                                        </th>
+                                        {
+                                            [
+                                                { 
+                                                    title: "Wallet: ALGO",
+                                                    src: ALGO_ICON,
+                                                    alt: "Algorand asset icon",
+                                                    sortKey: "wallet-algo",
+                                                },
+                                                {
+                                                    title: "Wallet: YLDY",
+                                                    src: YLDY_ICON,
+                                                    alt: "Yieldly asset icon",
+                                                    sortKey: "wallet-yldy",
+                                                },
+                                                {
+                                                    title: "NLL ALGO",
+                                                    src: ALGO_ICON,
+                                                    alt: "Algorand asset icon",
+                                                    sortKey: "staked-algo",
+                                                },
+                                                {
+                                                    title: "YLDY Staking",
+                                                    src: YLDY_ICON,
+                                                    alt: "Yieldly asset icon",
+                                                    sortKey: "staked-yldy",
+                                                }
+                                            ]
+                                            .map((data, index) => {
+                                                return (
+                                                    <th
+                                                        key={ "headings-col-" + index }>
+                                                        { data.title }
+                                                        <img
+                                                            className="mx-2"
+                                                            src={ data.src } 
+                                                            alt={ data.alt }
+                                                            height={ this.state.headerImageHeight }
+                                                            />
+                                                        <Button
+                                                            size="sm"
+                                                            variant={ this.state.sortBy === data.sortKey ? "info" : "primary"}
+                                                            onClick={(e) => this.setState({ sortBy: data.sortKey }, () => this.onUpdateSort() )}>
+                                                            <FontAwesomeIcon
+                                                                icon={faChevronUp} />
+                                                        </Button>
+                                                    </th>
+                                                )
+                                            })
+                                        }
                                     </tr>
                                 </thead>
                                 <tbody>
                                     {
-                                        this.state.stakingData.yieldlyData.map((data, index) => {
+                                        this.state.sortedStakingData.map((data, index) => {
+                                            // Determine index ranges for current pagination
                                             let rangeStart = this.state.pageSegment * (this.state.pageActive);
                                             let rangeEnd = (this.state.pageSegment * (this.state.pageActive + 1)) - 1;
                                             if (index < rangeStart || index > rangeEnd) {
                                                 return null;
                                             }
+                                            // Get values for current data address
                                             let walletAlgo = 0, walletYldy = 0, stakeAlgo = 0, stakeYldy = 0;
+                                            // Extract values from "assets" if exists
                                             if (data.assets) {
                                                 walletAlgo = data.assets.ALGO;
 
-                                                let yldyAssetKey = YLDY_ASSET_ID.toString();
+                                                let yldyAssetKey = constants.YLDY_ASSET_ID.toString();
                                                 if (data.assets[yldyAssetKey]) {
                                                     walletYldy = data.assets[yldyAssetKey];
                                                 }
                                             }
+                                            // Extract values from "stateData" if exists
                                             if (data.stateData) {
                                                 let nllKey = constants.NO_LOSS_LOTTERY_APP_ID.toString();
                                                 let yldyStakingKey = constants.YLDY_STAKING_APP_ID.toString();
@@ -293,7 +379,8 @@ class TopStakers extends Component {
                                 analyseStakingData(this.state.stakingData)
                                 .map((data, index) => {
                                     return (
-                                        <Col>
+                                        <Col
+                                            key={ "staking-data-" + index }>
                                             <Card
                                                 key={"staking-data-infos-" + index}
                                                 border="primary"
--- src/js/consts.js
+++ src/js/consts.js
@@ -1,4 +1,9 @@
 export const constants = {
+    // Algorand application id for the No Loss Lottery
     NO_LOSS_LOTTERY_APP_ID: 233725844,
+    // Algorand application id for YLDY staking
     YLDY_STAKING_APP_ID: 233725850,
+
+    // Algorand asset id for YLDY
+    YLDY_ASSET_ID: 226701642,
 };
