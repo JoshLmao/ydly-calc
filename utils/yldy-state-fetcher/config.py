26366d4233a56a11fe97b6aef84eda5075afd0bb
Josh Shepherd <josh_lmao@live.co.uk>
2021-11-04 22:26:01 +0000

    Updated state fetcher config

--- utils/yldy-state-fetcher/config.py
+++ utils/yldy-state-fetcher/config.py
@@ -9,6 +9,14 @@ yldyStakingApplicationID = 233725850
 opulStakingAppID = 348079765
 # YLDY/SMILE app id
 yldySmileStakingAppID = 352116819
+# OPUL/OPUL app id
+opulOpulAppID: 367431051
+# SMILE/SMILE
+smileSmileAppID: 373819681
+# YLDY/ARCC
+yldyArccAppID: 385089192
+# YLDY/GEMS
+yldyGemsAppID: 393388133
 
 # Dictionary of all application id's and their global state keys
 # to retrieve every X hours
@@ -26,6 +34,18 @@ APP_ID_STATE_KEYS_DICT = {
         "TYUL", "TYL", "GA", "GSS", "SA", "RA"
     ],
     yldySmileStakingAppID: [
+        "TYUL", "TYL", "GA", "GSS", "SA", "RA"
+    ],
+    opulOpulAppID: [
+        "TYUL", "TYL", "GA", "GSS", "SA", "RA"
+    ],
+    smileSmileAppID: [
+        "TYUL", "TYL", "GA", "GSS", "SA", "RA"
+    ],
+    yldyArccAppID: [ 
+        "TYUL", "TYL", "GA", "GSS", "SA", "RA"
+    ],
+    yldyGemsAppID: [
         "TYUL", "TYL", "GA", "GSS", "SA", "RA"
     ],
 }
